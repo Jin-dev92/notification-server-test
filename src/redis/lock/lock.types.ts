797e011f9925a7e7ccd32ed9d0ce64e22cd91948
Jin-devST <jindevst@gmail.com>
2026-05-27 23:04:04 +0900

    fix: LockAcquisitionError instanceof 안전성 보완

--- src/redis/lock/lock.types.ts
+++ src/redis/lock/lock.types.ts
@@ -3,5 +3,6 @@ export class LockAcquisitionError extends Error {
   constructor(key?: string) {
     super(key ? `락 획득 실패: ${key}` : '락 획득 실패');
     this.name = 'LockAcquisitionError';
+    Object.setPrototypeOf(this, LockAcquisitionError.prototype);
   }
 }
